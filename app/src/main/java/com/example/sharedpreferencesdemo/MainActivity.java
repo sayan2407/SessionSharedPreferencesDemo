76906ff09e9bb0383908219413d2140bcf9579ba
SAYAN PAL <sayanp166@gmail.com>
2020-08-03 14:37:48 +0530

    shared

---
+++ app/src/main/java/com/example/sharedpreferencesdemo/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.sharedpreferencesdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    UserSession session;
+
+    // Button Logout
+    Button btnLogout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Session class instance
+        session = new UserSession(getApplicationContext());
+
+        TextView lblName = (TextView) findViewById(R.id.lblName);
+        TextView lblEmail = (TextView) findViewById(R.id.lblEmail);
+
+        // Button logout
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        Toast.makeText(getApplicationContext(),
+                "User Login Status: " + session.isUserLoggedIn(),
+                Toast.LENGTH_LONG).show();
+
+
+
+        // Check user login
+        // If User is not logged in , This will redirect user to LoginActivity.
+        if(session.checkLogin())
+            finish();
+
+        // get user data from session
+        HashMap<String, String> user = session.getUserDetails();
+
+        // get name
+        String name = user.get(UserSession.KEY_NAME);
+
+        // get email
+        String email = user.get(UserSession.KEY_EMAIL);
+
+        // Show user data on activity
+        lblName.setText(Html.fromHtml("Name: <b>" + name + "</b>"));
+        lblEmail.setText(Html.fromHtml("Email: <b>" + email + "</b>"));
+
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // Clear the User session data
+                // and redirect user to LoginActivity
+                session.logoutUser();
+            }
+        });
+    }
+
+}
